3e9b22505f2a0c6b91042239bd2900d3f4512764
Fabian Aiolfi <fabianaiolfi@users.noreply.github.com>
2020-04-23 18:30:16 +0200

    Update detokenise.py

--- kaomoji/scripts/detokenise.py
+++ kaomoji/scripts/detokenise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-my_input = open("generated.txt", "r")
-my_output = open("detokenised.txt", "a")
+my_input = open("../data/generated.txt", "r")
+my_output = open("../data/detokenised.txt", "a")
 
 for line in my_input:
     eos_new_line = line.replace("<eos> ", "\n\n")
