1a27b191dc14b55d64194a8b43969aad102199c2
Fabian Aiolfi <fabianaiolfi@users.noreply.github.com>
2020-04-23 18:28:37 +0200

    Update tokenise.py

--- kaomoji/scripts/tokenise.py
+++ kaomoji/scripts/tokenise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-my_input = open("scraped.txt", "r")
-my_output = open("tokenised.txt", "a")
+my_input = open("../data/scraped.txt", "r")
+my_output = open("../data/tokenised.txt", "a")
 
 for line in my_input:
     my_output.write(" ".join(line))
